775aabd6d7261354f0e23bd3a976498c3b4d9788
Tiit Kuuskmäe <tiit.kuuskmae@gmail.com>
2023-07-31 15:40:06 +0300

    Implement difference hash algorithm

--- src/main/java/dev/brachtendorf/jimagehash_examples/CompareImages.java
+++ src/main/java/dev/brachtendorf/jimagehash_examples/CompareImages.java
@@ -2,13 +2,15 @@ package dev.brachtendorf.jimagehash_examples;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
 import dev.brachtendorf.jimagehash.hash.Hash;
-import dev.brachtendorf.jimagehash.hashAlgorithms.AverageHash;
+import dev.brachtendorf.jimagehash.hashAlgorithms.DifferenceHash;
 import dev.brachtendorf.jimagehash.hashAlgorithms.HashingAlgorithm;
 
 /**
@@ -20,24 +22,25 @@ import dev.brachtendorf.jimagehash.hashAlgorithms.HashingAlgorithm;
 public class CompareImages {
 
 	// Key bit resolution
-	private int keyLength = 64;
+	private int keyLength = 262144;
 
 	// Pick an algorithm
-	private HashingAlgorithm hasher = new AverageHash(keyLength);
+	private HashingAlgorithm hasher = new DifferenceHash(keyLength, DifferenceHash.Precision.Triple);
 
 	// Images used for testing
 	private HashMap<String, BufferedImage> images = new HashMap<>();
 
 	
-	public CompareImages() {
+	public CompareImages() throws IOException {
 
 		loadImages();
 
 		// Compare each picture to each other
+		String comparableName = "original";
+		BufferedImage comparableFile = ImageIO.read(new FileInputStream("images/original.png"));
+
 		images.forEach((imageName, image) -> {
-			images.forEach((imageName2, image2) -> {
-				formatOutput(imageName, imageName2, compareTwoImages(image, image2));
-			});
+			formatOutput(comparableName, imageName, compareTwoImages(image, comparableFile));
 		});
 	}
 
@@ -47,7 +50,7 @@ public class CompareImages {
 	 * @param image2	The second image
 	 * @return	true if the algorithm defines the images to be similar.
 	 */
-	public boolean compareTwoImages(BufferedImage image1, BufferedImage image2) {
+	public double compareTwoImages(BufferedImage image1, BufferedImage image2) {
 
 		//Generate the hash for each image
 		Hash hash1 = hasher.hash(image1);
@@ -55,54 +58,32 @@ public class CompareImages {
 
 		//Compute a similarity score
 		// Ranges between 0 - 1. The lower the more similar the images are.
-		double similarityScore = hash1.normalizedHammingDistance(hash2);
-
-		return similarityScore < 0.4d;
-	}
-	
-	/**
-	 * Compares the similarity of two images.
-	 * @param image1	First image to be matched against 2nd image
-	 * @param image2	The second image
-	 * @return	true if the algorithm defines the images to be similar.
-	 * @throws IOException IOerror occurred during image loading
-	 */
-	public boolean compareTwoImages2(File image1, File image2) throws IOException {
-
-		//Generate the hash for each image
-		Hash hash1 = hasher.hash(image1);
-		Hash hash2 = hasher.hash(image2);
-
-		// Ranges between [0 - keyLength]. The lower the more similar the images are.
-		int similarityScore = hash1.hammingDistance(hash2);
-
-		return similarityScore < 41;
+		return hash1.normalizedHammingDistance(hash2);
 	}
 
-	
 	//Utility function
-	private void formatOutput(String image1, String image2, boolean similar) {
-		String format = "| %-11s | %-11s | %-8b |%n";
+	private void formatOutput(String image1, String image2, double similar) {
+		String format =  "%-11s | %-11s | %-11s %n";
 		System.out.printf(format, image1, image2, similar);
 	}
 	
 	private void loadImages() {
 		// Load images
 		try {
-			images.put("ballon", ImageIO.read(new File("src/main/resources/images/ballon.jpg")));
-			images.put("copyright", ImageIO.read(new File("src/main/resources/images/copyright.jpg")));
-			images.put("highQuality", ImageIO.read(new File("src/main/resources/images/highQuality.jpg")));
-			images.put("lowQuality", ImageIO.read(new File("src/main/resources/images/lowQuality.jpg")));
-			images.put("thumbnail", ImageIO.read(new File("src/main/resources/images/thumbnail.jpg")));
+			for(int i = 1; i <= 10; i++){
+				String pointer = String.valueOf(i);
+				images.put("sample_" + pointer, ImageIO.read(new FileInputStream("images/sample_" + pointer + ".jpg")));
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		//Print header
-		System.out.println("|   Image 1   |   Image 2   | Similar  |");
+		System.out.println("|   Image 1   |   Image 2   | Result  |");
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		new CompareImages();
 	}
 
